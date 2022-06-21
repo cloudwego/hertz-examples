0f7c8b7a029c8a30b6b09f51876b1b0e10562d84
范广宇 <fanguangyu.fgy1995@bytedance.com>
2022-06-21 12:28:01 +0800

    feat: add package template

---
+++ hz/tempalte/package/go.mod
@@ -0,0 +1,3 @@
+module github.com/hertz/hello
+replace github.com/apache/thrift => github.com/apache/thrift v0.13.0
+			
---
+++ hz/tempalte/package/handler_test/hello/example/hello_service.go
@@ -0,0 +1,27 @@
+// this is my custom handler.
+
+package example
+
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	example "github.com/hertz/hello/biz/model/hello/example"
+)
+
+// HelloMethod .
+// @router /hello [GET]
+func HelloMethod(ctx context.Context, c *app.RequestContext) {
+	//  you can code something
+	var err error
+	var req example.HelloReq
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	resp := new(example.HelloResp)
+
+	c.JSON(200, resp)
+}
