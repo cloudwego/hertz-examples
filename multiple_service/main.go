4b73d9979e4ec33715bcd8520a84e425b6d07b34
Skyenought <Skyenought@github.com>
2022-07-20 11:23:10 +0800

    feat: add multiple_service example

---
+++ multiple_service/main.go
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"sync"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+var wg sync.WaitGroup
+
+func hertz1() {
+	h := server.Default(server.WithHostPorts(":8080"))
+	h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+		ctx.JSON(consts.StatusOK, utils.H{"ping": "pong1"})
+	})
+	h.Spin()
+}
+
+func hertz2() {
+	h := server.Default(server.WithHostPorts(":8081"))
+	h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+		ctx.JSON(consts.StatusOK, utils.H{"ping": "pong2"})
+	})
+	h.Spin()
+}
+
+func main() {
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		hertz1()
+	}()
+	go func() {
+		defer wg.Done()
+		hertz2()
+	}()
+	wg.Wait()
+}
