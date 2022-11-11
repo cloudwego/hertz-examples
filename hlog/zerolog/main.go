77fd70d5474db05e8c9fc654472c0919f65d702f
L2ncE <llance_24@foxmail.com>
2022-11-11 11:48:19 +0800

    feat: add zerolog example

---
+++ hlog/zerolog/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"path"
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	hertzZerolog "github.com/hertz-contrib/logger/zerolog"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+func main() {
+	h := server.Default()
+
+	// Customizable output directory.
+	var logFilePath string
+	dir := "./hlog"
+	logFilePath = dir + "/logs/"
+	if err := os.MkdirAll(logFilePath, 0o777); err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	// Set filename to date
+	logFileName := time.Now().Format("2006-01-02") + ".log"
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			log.Println(err.Error())
+			return
+		}
+	}
+
+	// For zap detailed settings, please refer to https://github.com/hertz-contrib/logger/tree/main/zerolog and https://github.com/rs/zerolog
+	logger := hertzZerolog.New()
+	// Provides compression and deletion
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   fileName,
+		MaxSize:    20,   // A file can be up to 20M.
+		MaxBackups: 5,    // Save up to 5 files at the same time.
+		MaxAge:     10,   // A file can exist for a maximum of 10 days.
+		Compress:   true, // Compress with gzip.
+	}
+
+	logger.SetOutput(lumberjackLogger)
+	logger.SetLevel(hlog.LevelDebug)
+
+	hlog.SetLogger(logger)
+
+	h.GET("/hello", func(ctx context.Context, c *app.RequestContext) {
+		hlog.Info("Hello, hertz")
+		c.String(consts.StatusOK, "Hello hertz!")
+	})
+
+	h.Spin()
+}
