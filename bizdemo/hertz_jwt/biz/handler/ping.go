dce0a76ed2d9bdf6c1f781e973724dec6ec8c491
BaiZe1998 <1157467179@qq.com>
2022-11-16 13:18:16 +0800

    optimize: append IdentityHandler for extracting token information

--- bizdemo/hertz_jwt/biz/handler/ping.go
+++ bizdemo/hertz_jwt/biz/handler/ping.go
@@ -22,15 +22,16 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/cloudwego/hertz-examples/bizdemo/hertz_jwt/biz/model"
+	"github.com/cloudwego/hertz-examples/bizdemo/hertz_jwt/biz/mw"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/common/utils"
-	"github.com/hertz-contrib/jwt"
 )
 
 // Ping .
 func Ping(ctx context.Context, c *app.RequestContext) {
-	userId, _ := c.Get(jwt.IdentityKey)
+	user, _ := c.Get(mw.IdentityKey)
 	c.JSON(200, utils.H{
-		"message": fmt.Sprintf("user_id:%v", userId),
+		"message": fmt.Sprintf("username:%v", user.(*model.User).UserName),
 	})
 }
