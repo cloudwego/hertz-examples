f666e3338e2ab15ecd99c940b4dc2a470746836d
清 <claude-zq@qq.com>
2023-01-09 22:06:47 +0800

    fix: add license to each file and replaced obsolete function

--- middleware/csrf/custom_keylookup/main.go
+++ middleware/csrf/custom_keylookup/main.go
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2022 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package main
 
 import (
 	"context"
+
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/hertz-contrib/csrf"
@@ -13,7 +29,7 @@ func main() {
 	h := server.Default()
 
 	store := cookie.NewStore([]byte("store"))
-	h.Use(sessions.Sessions("csrf-session", store))
+	h.Use(sessions.New("csrf-session", store))
 	h.Use(csrf.New(csrf.WithKeyLookUp("form:csrf")))
 
 	h.GET("/protected", func(c context.Context, ctx *app.RequestContext) {
