a8653145e73bb71df5e92121b7ca0da63ee7ed0f
L2ncE <llance_24@foxmail.com>
2023-02-23 09:29:36 +0800

    feat: add loadbalance middleware example

---
+++ middleware/loadbalance/round_robin/client/main.go
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/app/client"
+	"github.com/cloudwego/hertz/pkg/app/client/loadbalance"
+	"github.com/cloudwego/hertz/pkg/app/middlewares/client/sd"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	loadbalanceEx "github.com/hertz-contrib/loadbalance/round_robin"
+	"github.com/hertz-contrib/registry/nacos"
+)
+
+func main() {
+	cli, err := client.NewClient()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	lb := loadbalanceEx.NewRoundRobinBalancer()
+	opt := loadbalance.Options{
+		RefreshInterval: 10 * time.Second,
+		ExpireInterval:  25 * time.Second,
+	}
+	r, err := nacos.NewDefaultNacosResolver()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	cli.Use(sd.Discovery(r, sd.WithLoadBalanceOptions(lb, opt)))
+	for i := 0; i < 10; i++ {
+		status, body, err := client.Get(context.Background(), nil, "http://hertz.test.demo/ping")
+		if err != nil {
+			hlog.Fatal(err)
+		}
+		hlog.Infof("code=%d,body=%s\n", status, string(body))
+	}
+}
---
+++ middleware/loadbalance/round_robin/server/main.go
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/app/server/registry"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/hertz-contrib/registry/nacos"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		num := i
+		go func() {
+			addr := fmt.Sprintf("127.0.0.1:800%d", num)
+			r, err := nacos.NewDefaultNacosRegistry()
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			h := server.Default(
+				server.WithHostPorts(addr),
+				server.WithRegistry(r, &registry.Info{
+					ServiceName: "hertz.test.demo",
+					Addr:        utils.NewNetAddr("tcp", addr),
+					Weight:      10,
+					Tags:        nil,
+				}),
+			)
+			h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+				ctx.JSON(consts.StatusOK, utils.H{"addr": addr})
+			})
+			h.Spin()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
