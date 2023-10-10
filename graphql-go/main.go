99195077196912c127fb927a2ab90b66cf0915f5
Skyenought <skyenought@qq.com>
2023-10-10 17:55:05 +0800

    update

--- graphql-go/main.go
+++ graphql-go/main.go
@@ -18,11 +18,11 @@ package main
 
 import (
 	"context"
+
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
-
 	"github.com/graphql-go/graphql"
 )
 
