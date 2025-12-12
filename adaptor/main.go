2f8d4ad282c9d688dc03298894ea8d7cbce8c1c6
joshua.ho@bytedance.com <joshua.ho@bytedance.com>
2025-12-12 14:58:14 +0800

    chore: update adaptor examples

--- adaptor/main.go
+++ adaptor/main.go
@@ -17,11 +17,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
-	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/common/adaptor"
 )
@@ -39,20 +37,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	h := server.Default()
 
-	h.GET("/hello", func(ctx context.Context, c *app.RequestContext) {
-		req, err := adaptor.GetCompatRequest(&c.Request)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		// You may build more logic on req
-		fmt.Println(req.URL.String())
-
-		// caution: don't pass in c.GetResponse() as it return a copy of response
-		rw := adaptor.GetCompatResponseWriter(&c.Response)
-
-		handler(rw, req)
-	})
+	// Wrap the standard http.HandlerFunc using HertzHandler
+	h.GET("/hello", adaptor.HertzHandler(http.HandlerFunc(handler)))
 
 	h.Spin()
 }
